edde3e30d11d289c57391feee853f1899e59dbd3
Maxim Makovskiy <makovskiyms@gmail.com>
2022-10-09 17:28:48 +0600

    Add the ability to use decimal

--- static/script.js
+++ static/script.js
@@ -6,12 +6,14 @@ const historyDisplay = document.querySelector("#history");
 const currentDisplay = document.querySelector("#current");
 const clearButton = document.querySelector("#clear");
 const backspaceButton = document.querySelector("#backspace");
+const decimalButton = document.querySelector("#decimal");
 const equalsButton = document.querySelector("#equals");
 const numberButtons = document.querySelectorAll(".number");
 const operatorButtons = document.querySelectorAll(".operator");
 
 clearButton.addEventListener("click", clearScreen);
 backspaceButton.addEventListener("click", erase);
+decimalButton.addEventListener("click", addDecimal);
 equalsButton.addEventListener("click", evaluate);
 numberButtons.forEach(button => button.addEventListener("click", getNumber));
 operatorButtons.forEach(op => op.addEventListener("click", getOperator));
@@ -22,6 +24,18 @@ function getNumber() {
     reset = false;
   }
   currentDisplay.textContent += this.value;
+}
+
+function addDecimal() {
+  if (currentDisplay.textContent.includes(".")) {
+    return
+  }
+  if (reset) {
+    currentDisplay.textContent = "0.";
+    reset = false;
+  } else {
+    currentDisplay.textContent += ".";
+  }
 }
 
 function getOperator() {
